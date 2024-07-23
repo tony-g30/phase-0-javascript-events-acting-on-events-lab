998f7a7bc9c3b768fea03b203007d8b01ef8e8f3
tony-g30 <tonyaudo@gmail.com>
2024-07-23 08:47:25 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) { // 400px (game width) - 40px (dodger width) = 360px
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'ArrowLeft') {
+    moveDodgerLeft();
+  } else if (event.key === 'ArrowRight') {
+    moveDodgerRight();
+  }
+});
